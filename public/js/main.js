69e5cb47cba9d87efa4e9eb6f43ec4106a9520b7
Akshay Kumar <akshay.nm92@gmail.com>
2021-06-06 09:07:29 +0530

    clean slate

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'animations/data.json'
-  })
-}
